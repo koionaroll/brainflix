b94cc38393d44ed42352c40aebc6ef064ac53e5e
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-01 09:10:29 -0800

    finished description section

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,17 +1,24 @@
 import "./Description.scss"
 
+let date = "07/11/2021"
+let views = "1,001,023"
+let likes = "110,985"
+let author = "Red Crow"
+let title = "BMX Rampage: 2021 Highlights"
+
+
 const Description = () => {
   return (
     <div className="description">
-        <h1 className="description__title">BMX Rampage: 2021 Highlights</h1>
+        <h1 className="description__title">{title}</h1>
         <div className="description__container">
             <div className="description__stats1">
-                <p className="description__author">By Red Crow</p>
-                <p className="description__date">07/11/2021</p>
+                <p className="description__author">By {author}</p>
+                <p className="description__date">{date}</p>
             </div>
             <div className="description__stats2">
-                <div className="description__views">1,001,023</div>
-                <div className="description__likes">110,985</div>
+                <div className="description__views">{views}</div>
+                <div className="description__likes">{likes}</div>
             </div>
         </div>
         <p className="description__synopsis">
