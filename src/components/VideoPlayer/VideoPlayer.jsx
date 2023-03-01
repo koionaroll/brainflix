d72aa1de3459e2e096695e423df27dcd54d264f6
Khoi Tran <tranvankhoi2002@gmail.com>
2023-02-28 23:13:39 -0800

    added placeholder for video player

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,9 @@
+import "./VideoPlayer.scss"
+
+const VideoPlayer = () => {
+  return (
+    <div className="video-player">VideoPlayer This is a Placeholder</div>
+  )
+}
+
+export default VideoPlayer
