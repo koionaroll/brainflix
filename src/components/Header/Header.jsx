654728a3bfb26b0318b69154cb1dedacca617f86
Khoi Tran <tranvankhoi2002@gmail.com>
2023-02-28 22:55:18 -0800

    finished header for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
 import "./Header.scss"
+import logo from "../../assets/logo/BrainFlix-logo.svg"
+import profile from "../../assets/images/Mohan-muruge.jpg"
 
-function Header(){
+function Header() {
 
-    return(
-        <div className="test">hello world</div>
+    return (
+        <>
+            <div className="header">
+                <img className="header__logo" src={logo} alt="logo" />
+                <div className="header__container">
+                    <textarea className="header__search" placeholder="Search"></textarea>
+                    <img className="header__profile" src={profile} alt="profile" />
+                </div>
+                <a className="header__upload" href="../../App.jsx" type= "submit">
+                    UPLOAD
+                </a>
+            </div>
+        </>
     )
 }
 
