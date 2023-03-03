32cf40f1734bd1c098ebfc0bc8ad3278ee80f242
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-03 14:33:12 -0800

    added dynamic comment counter

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,38 +1,41 @@
-import "./Comments.scss"
-import profile from "../../assets/images/Mohan-muruge.jpg"
+import "./Comments.scss";
+import profile from "../../assets/images/Mohan-muruge.jpg";
 
-
-function Comments ({selectedVid}){
-    let numOfComments = 3
-    const commentsInfo = selectedVid
+function Comments({ selectedVid }) {
+    const commentsInfo = selectedVid;
 
   return (
     <section className="comments">
-        <h2 className="comments-counter">{numOfComments} Comments</h2>
-        <div className="comments-submit">
-            <img className="comments-submit__profile" src={profile} alt="pfp"/>
-            <div className="comments-submit__wrapper">  
-                <h3 className="comments-submit__label">JOIN THE CONVERSATION</h3>
-                <textarea className="comments-submit__textarea" placeholder="Add a new comment"></textarea>
-                <button className="comments-submit__btn">COMMENT</button>
-            </div>
+      <h2 className="comments-counter">{commentsInfo.length} Comments</h2>
+      <div className="comments-submit">
+        <img className="comments-submit__profile" src={profile} alt="pfp" />
+        <div className="comments-submit__wrapper">
+          <h3 className="comments-submit__label">JOIN THE CONVERSATION</h3>
+          <textarea
+            className="comments-submit__textarea"
+            placeholder="Add a new comment"
+          ></textarea>
+          <button className="comments-submit__btn">COMMENT</button>
         </div>
-        {commentsInfo.map((element) => { 
+      </div>
+      {commentsInfo.map((element) => {
         return (
-        <div className="comments-list" key={element.id}>
+          <div className="comments-list" key={element.id}>
             <div className="comments-list__profile"></div>
             <div className="comments-list--column">
-                <div className="comments-list--row">
-                    <p className="comments-list__name">{element.name}</p>
-                    <p className="comments-list__date">{new Date(element.timestamp).toLocaleDateString()}</p>
-                </div>
-                <p className="comments-list__content">{element.comment}</p>
+              <div className="comments-list--row">
+                <p className="comments-list__name">{element.name}</p>
+                <p className="comments-list__date">
+                  {new Date(element.timestamp).toLocaleDateString()}
+                </p>
+              </div>
+              <p className="comments-list__content">{element.comment}</p>
             </div>
-        </div>
-            )
-        })}
+          </div>
+        );
+      })}
     </section>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
